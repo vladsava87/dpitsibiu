8eab6daf7fbfcf33fecd76b1ba5f1f7fb725f562
Unknown <berti_florea@yahoo.com>
2018-06-25 16:31:38 +0300

    Clase Tabele Partea de sus

--- EntityFrameworkText/DatabaseLayer/DataModels/Model.cs
+++ EntityFrameworkText/DatabaseLayer/DataModels/Model.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using DatabaseLayer.DataModels;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
@@ -93,5 +95,52 @@ namespace DatabaseLayer.DataModels
         
 
         public virtual t_materie Materie { get; set; }
+    }
+    public class t_elev
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public DateTime Date { get; set; }
+        public int Telefon { get; set; }
+        public string Email { get; set; }
+        public int Numar_Matricol { get; set; }
+        public int Clasa { get; set; }
+
+        public virtual List<t_nota> Note { get; set; }
+        public virtual List<t_elev> Absente { get; set; }
+        public virtual List<t_observatii> Observatii { get; set; }
+    }
+
+    public class t_nota
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual List<t_materie> Materie { get; set; }
+        public double Nota { get; set; }
+        public bool Teza { get; set; }
+        public Sem { get; set; }
+        public t_elev Elev { get; set; }
+    }
+
+    public class t_clasa
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Numar { get; set; }
+        public string Serie { get; set; }
+        public int An { get; set; }
+
+        public virtual List<t_profil> Profil { get; set; }
+        public virtual List<t_profesor> Diriginte { get; set; }
+        public virtual List<t_elev> Elevi { get; set; }
+    }
+
+    public class t_profil
+    {
+        [Key]
+        public int Id { get; set; }
+        string Nume { get; set; }
+        public virtual t_profil Profil { get; set; }
     }
 }
