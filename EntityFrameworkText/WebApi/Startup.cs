ac818a7ba0b59bc32029036c989cff4c53529d2e
Unknown <berti_florea@yahoo.com>
2018-07-11 09:47:36 +0300

    STers WebApi vechi

--- EntityFrameworkText/WebApi/Startup.cs
+++ EntityFrameworkText/WebApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatabaseLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,7 +21,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WebApiContext>(opt =>
+            services.AddDbContext<CatalogContex>(opt =>
                 opt.UseInMemoryDatabase("WebApiList"));
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
