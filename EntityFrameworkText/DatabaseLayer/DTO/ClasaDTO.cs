ea203bfe1f5f6321f25742d50026ab68db0f0815
Vlad S <vlad.ic.sava@gmail.com>
2018-07-11 11:31:08 +0300

    DTO objects added

---
+++ EntityFrameworkText/DatabaseLayer/DTO/ClasaDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DatabaseLayer.DTO
+{
+    [DataContract]
+    public class ClasaDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public int Numar { get; set; }
+        [DataMember]
+        public string Serie { get; set; }
+        [DataMember]
+        public int An { get; set; }
+        [DataMember]
+        public int ProfilID { get; set; }
+        [DataMember]
+        public ProfilDTO Profil { get; set; }
+        [DataMember]
+        public int DiriginteID { get; set; }
+        //public virtual t_profesor Diriginte { get; set; }
+        //public virtual List<t_elev> Elevi { get; set; }
+    }
+}
---
+++ EntityFrameworkText/DatabaseLayer/DTO/ProfilDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DatabaseLayer.DTO
+{
+    [DataContract]
+    public class ProfilDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nume { get; set; }
+        [DataMember]
+        public List<ClasaDTO> Clase { get; set; }
+    }
+}
