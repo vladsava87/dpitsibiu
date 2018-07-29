13eac5971071b56a21ce3048247ebd75ff6301bb
Unknown <toth.cristina00@yahoo.com>
2018-07-29 22:32:25 +0300

    DTO elev

--- EntityFrameworkText/DatabaseLayer/DTO/ElevDTO.cs
+++ EntityFrameworkText/DatabaseLayer/DTO/ElevDTO.cs
@@ -15,10 +15,26 @@ namespace DatabaseLayer.DTO
         public string Nume { get; set; }
         [DataMember]
         public string Prenume { get; set; }
+        [DataMember]
+        public int Telefon { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public int NumarMatricol { get; set; }
+        [DataMember]
+        public int Clasa { get; set; }
 
         [DataMember]
         public int NotaID { get; set; }
         [DataMember]
         public List<NotaDTO> Note { get; set; }
+        [DataMember]
+        public int AbsentaID { get; set; }
+        [DataMember]
+        public List<AbsentaDTO> Absente { get; set; }
+        [DataMember]
+        public int ObservatieID { get; set; }
+        [DataMember]
+        public List<ObservatieDTO> Observatii { get; set; }
     }
 }
