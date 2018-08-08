171dad7bdeb0816012749bb2a316107053eaef11
Unknown <toth.cristina00@yahoo.com>
2018-08-08 20:57:22 +0300

    Controller Elev+Profil

--- EntityFrameworkText/NewWebService/Controllers/ElevController.cs
+++ EntityFrameworkText/NewWebService/Controllers/ElevController.cs
@@ -1,8 +1,11 @@
-﻿using DatabaseLayer;
+﻿using AutoMapper;
+using DatabaseLayer;
+using DatabaseLayer.DataModels;
+using DatabaseLayer.DTO;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -15,28 +18,83 @@ namespace NewWebService.Controllers
         // GET: api/Elev
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            var elevi = catalog.Elevi.ToList();
+
+            var televi = Mapper.Map<List<ElevDTO>>(elevi);
+
+            return televi;
         }
 
         // GET: api/Elev/5
         public string Get(int id)
         {
-            return "value";
+            var elev = catalog.Elevi.Where(e => e.Id == id).FirstOrDefault();
+
+            var telev = Mapper.Map<ElevDTO>(elev);
+
+            return telev;
         }
 
         // POST: api/Elev
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post(HttpRequestMessage request)
         {
+            var msg = new HttpResponseMessage();
+
+            try
+            {
+                var value = request.Content.ReadAsStringAsync().Result;
+
+                ElevDTO elev = JsonConvert.DeserializeObject<ElevDTO>(value);
+                t_elev elevnou = Mapper.Map<ElevDTO, t_elev>(elev);
+
+                catalog.Elevi.Add(elevnou);
+                catalog.SaveChanges();
+
+                msg.StatusCode = System.Net.HttpStatusCode.OK;
+                msg.Content = new StringContent("POST Request performed successfully");
+            }
+            catch (Exception e)
+            {
+                msg.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                msg.Content = new StringContent("POST Request could not be performed");
+            }
+
+            return msg;
         }
 
         // PUT: api/Elev/5
         public void Put(int id, [FromBody]string value)
         {
+            var value = request.Content.ReadAsStringAsync().Result;
+
+            t_elev Elev = catalog.Elevi.Where(e => e.Id == id).FirstOrDefault();
+            ElevDTO elevnou = JsonConvert.DeserializeObject<ElevDTO>(value);
+           
+            Elev.Id = elevnou.Id;
+            Elev.Nume = elevnou.Nume;
+            Elev.Prenume = elevnou.Prenume;
+            Elev.DataNastere = elevnou.DataNestere;
+            Elev.Telefon = elevnou.Telefon;
+            Elev.Email = elevnou.Email;
+            Elev.NumarMatricol = elevnou.NumarMatricol;
+            Elev.Clasa = elevnou.Clasa;
+            
+            t_nota Nota = catalog.Note.Where(n => n.Id == elevnou.Id).FirstOrDefault();
+            Elev.Nota = Nota;
+            
+            t_elev Absenta = catalog.Absente.Where(a => a.Id == elevnou.Id).FirstOrDefault();
+            Elev.Absenta = Absenta;
+
+            t_elev Observatie = catalog.Observatii.Where(o => o.Id == elevnou.Id).FirstOrDefault();
+            Elev.Observatie = Observatie;
         }
 
         // DELETE: api/Elev/5
         public void Delete(int id)
         {
+            t_elev Elev = catalog.Elevi.Where(e => e.Id == id).FirstOrDefault();
+            catalog.Elevi.Remove(Elev);
+            catalog.SaveChanges();
         }
     }
 }
--- EntityFrameworkText/NewWebService/Controllers/ProfilController.cs
+++ EntityFrameworkText/NewWebService/Controllers/ProfilController.cs
@@ -1,9 +1,13 @@
-﻿using DatabaseLayer;
+﻿using AutoMapper;
+using DatabaseLayer;
+using DatabaseLayer.DataModels;
+using DatabaseLayer.DTO;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
-using AutoMapper;
-using DatabaseLayer.DTO;
 
 namespace NewWebService.Controllers
 {
@@ -24,22 +28,58 @@ namespace NewWebService.Controllers
         // GET: api/Profil/5
         public string Get(int id)
         {
-            return "value";
+            var profil = catalog.Profiluri.Where(p => p.Id == id).FirstOrDefault();
+
+            var tprofil = Mapper.Map<ProfilDTO>(profil);
+
+            return tprofil;
         }
 
         // POST: api/Profil
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post(HttpRequestMessage request)
         {
+            var msg = new HttpResponseMessage();
+
+            try
+            {
+                var value = request.Content.ReadAsStringAsync().Result;
+
+                ProfilDTO profil = JsonConvert.DeserializeObject<ProfilDTO>(value);
+                t_profil profilnou = Mapper.Map<ElevDTO, t_profil>(profil);
+
+                catalog.Profiluri.Add(profilnou);
+                catalog.SaveChanges();
+
+                msg.StatusCode = System.Net.HttpStatusCode.OK;
+                msg.Content = new StringContent("POST Request performed successfully");
+            }
+            catch (Exception e)
+            {
+                msg.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                msg.Content = new StringContent("POST Request could not be performed");
+            }
+
+            return msg;
         }
 
         // PUT: api/Profil/5
         public void Put(int id, [FromBody]string value)
         {
+            var value = request.Content.ReadAsStringAsync().Result;
+
+            t_profil Profil = catalog.Profiluri.Where(p => p.Id == id).FirstOrDefault();
+            ProfilDTO profilnou = JsonConvert.DeserializeObject<ProfilDTO>(value);
+
+            Profil.Id = profilnou.Id;
+            Profil.Nume = profilnou.Nume;
         }
 
         // DELETE: api/Profil/5
         public void Delete(int id)
         {
+            t_profil Profil = catalog.Profiluri.Where(p => p.Id == id).FirstOrDefault();
+            catalog.Profiluri.Remove(Profil);
+            catalog.SaveChanges();
         }
     }
 }
