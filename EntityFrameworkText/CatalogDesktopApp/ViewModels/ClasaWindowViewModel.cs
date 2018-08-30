01f6137d0d30067cb32fafa8d6593b6bd7f9210e
Unknown <berti_florea@yahoo.com>
2018-08-30 16:45:59 +0300

    Listbox clasa partial

--- EntityFrameworkText/CatalogDesktopApp/ViewModels/ClasaWindowViewModel.cs
+++ EntityFrameworkText/CatalogDesktopApp/ViewModels/ClasaWindowViewModel.cs
@@ -14,10 +14,36 @@ namespace CatalogDesktopApp.ViewModels
     public class ClasaWindowViewModel : ViewModelBase
     {
         public ClasaDTO Clasa { get; set; }
+
+        private List<ElevTest> elevi = new List<ElevTest>();
+
+
         
         public ClasaWindowViewModel()
         {
             EleviCommand = new RelayCommand(ListElevi);
+
+            var temp = new List<ElevTest>();
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+            temp.Add(new ElevTest { Nume = "Vlad", Prenume = "Sava" });
+
+            Elevi = temp;
+        }
+
+        public List<ElevTest> Elevi
+        {
+            get => elevi;
+
+            set
+            {
+                elevi = value;
+                OnPropertyChanged("Elevi");
+            }
         }
 
         public void ListElevi(object obj)
@@ -27,12 +53,13 @@ namespace CatalogDesktopApp.ViewModels
 
         public ICommand EleviCommand { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        
+    }
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    public class ElevTest
+    {
+        public string Nume { get; set; }
+
+        public string Prenume { get; set; }
     }
 }
